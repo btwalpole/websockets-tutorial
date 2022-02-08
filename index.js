f4b08bb7c96c7caf6c57b6be46707d5f31efcb45
Ben Walpole <benwalpole95@gmail.com>
2022-02-08 17:08:41 +0000

    chat working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io')
 
 const app = express()
 const server = app.listen(4000, function () {
@@ -7,3 +8,14 @@ const server = app.listen(4000, function () {
 
 // Serve Static files
 app.use(express.static('public'));
+
+//Socket setup
+const io = socket(server);
+
+io.on('connection', function (socket) {
+    console.log('made socket connection', socket.id)
+
+    socket.on('chat', function (data) {
+        io.sockets.emit('chat', data)
+    })
+})
