7b14d1946b7ef9b7da9f71ba4e35b852f8e9177e
Ben Walpole <benwalpole95@gmail.com>
2022-02-13 14:29:16 +0000

    adding port as env variable

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const socket = require('socket.io')
+const express = require("express");
+const socket = require("socket.io");
 
-const app = express()
-const server = app.listen(4000, function () {
-    console.log('listening to requests on port 4000')
-})
+const app = express();
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, function () {
+  console.log("listening to requests on port 4000");
+});
 
 // Serve Static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Socket setup
 const io = socket(server);
 
-io.on('connection', function (socket) {
-    console.log('made socket connection', socket.id)
+io.on("connection", function (socket) {
+  console.log("made socket connection", socket.id);
 
-    socket.on('chat', function (data) {
-        io.sockets.emit('chat', data)
-    })
+  socket.on("chat", function (data) {
+    io.sockets.emit("chat", data);
+  });
 
-    socket.on('typing', function (data) {
-        socket.broadcast.emit('typing', data)
-    })
-})
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
+});
