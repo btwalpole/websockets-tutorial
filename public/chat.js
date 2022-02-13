37d64d0113b03301820fb2aaf42d77ced9b5e44c
Ben Walpole <benwalpole95@gmail.com>
2022-02-13 18:34:11 +0000

    updating to buzzer

--- public/chat.js
+++ public/chat.js
@@ -4,8 +4,8 @@ const socket = io();
 
 //Query DOM
 var message = document.getElementById("message"),
-  handle = document.getElementById("handle"),
-  btn = document.getElementById("send"),
+  name = document.getElementById("name"),
+  btn = document.getElementById("buzz"),
   output = document.getElementById("output"),
   feedback = document.getElementById("feedback");
 
@@ -13,19 +13,19 @@ var message = document.getElementById("message"),
 btn.addEventListener("click", function () {
   socket.emit("chat", {
     message: message.value,
-    handle: handle.value,
+    name: name.value,
   });
 });
 
 message.addEventListener("keypress", function () {
-  socket.emit("typing", handle.value);
+  socket.emit("typing", name.value);
 });
 
 //Listen for events
 socket.on("chat", function (data) {
   feedback.innerHTML = "";
   output.innerHTML +=
-    "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+    "<p><strong>" + data.name + ": </strong>" + data.message + "</p>";
 });
 
 socket.on("typing", function (data) {
