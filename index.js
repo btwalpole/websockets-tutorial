729c1ac778233d20e4164b72916a7450e18db7d1
Ben Walpole <btm.walpole@gmail.com>
2022-04-12 15:02:10 +0000

    updated room code generator to have only capital letters and numbers

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ io.on("connection", function (socket) {
 function makeid(length) {
   var result = "";
   var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   var charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
